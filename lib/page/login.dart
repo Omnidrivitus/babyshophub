6ce0ff81091700bdb8fab304a9b07e77d7cea5ff
Omnidrivitus <vitusjj12@gmail.com>
2026-04-04 20:24:41 +0100

    feat: LoginPage

---
+++ lib/page/login.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              //Logo
+              Icon(Icons.lock, size: 100),
+              const SizedBox(height: 50),
+
+              //Phrase
+              Text(
+                "Welcome to Babify",
+                style: TextStyle(color: Colors.grey[700], fontSize: 16),
+              ),
+              const SizedBox(height: 25),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
