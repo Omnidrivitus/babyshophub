59f98a436b06e52cc459c3381c3090eba2ca0881
Omnidrivitus <vitusjj12@gmail.com>
2026-04-19 23:54:49 +0100

    feat: Hope page update

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,10 +1,37 @@
+import 'package:babyshophub/data/repository/auth_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  void onPressed(BuildContext context) async {
+    try {
+      await PreferencesRepository().clearToken();
+      if (context.mounted) context.go('/login');
+    } catch (e) {
+      if (context.mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('There was an error, try again')),
+        );
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Home Page")));
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Text("Home Page"),
+            IconButton(
+              onPressed: () => onPressed(context),
+              icon: const Icon(Icons.logout),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
