a1a265682fd3a174447d74d3d1081a64a8a90993
Omnidrivitus <vitusjj12@gmail.com>
2026-04-20 14:05:27 +0100

    fix structure in Home

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -6,7 +6,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Column(children: [Text("Home Page")])),
+      body: SafeArea(
+        child: Center(child: Column(children: [Text("Home Page")])),
+      ),
     );
   }
 }
